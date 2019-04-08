63e70e867c68827a1c9f2b883b0428ce54eb76a4
wen <1830759297@qq.com>
2019-04-08 08:57:48 +0800

    关键字搜索接口完成

--- route/products/index.js
+++ route/products/index.js
@@ -9,18 +9,38 @@ var pool = mysql.createPool({
   database: "mymall_db"
 })
 
-//精选好物接口、
+//精选好物、相关推荐接口
 productsApp.get('/', function (req, res) {
   pool.query("select * from goodShop", function (err, data) {
-    console.log(data);
-    console.log(typeof data);
     res.send({
       resultCode: 0,
       resultMsg: "success",
       list: data
     }).end();
-
   })
 })
-
+//新品推荐接口
+productsApp.get('/latest', function (req, res) {
+  pool.query("select * from newPing", function (err, data) {
+    res.send({
+      resultCode: 0,
+      resultMsg: "success",
+      list: data
+    }).end();
+  })
+})
+//关键字搜索接口
+productsApp.get('/keyword', function (req, res) {
+  console.log(req.query.itemName);
+  pool.query(`select mainPic,name,sellPrice,saleProps from allGoods where keyword like '%${req.query.itemName}%'`, function (err, data) {
+    console.log(data);
+    res.send({
+      resultCode: 0,
+      resultMsg: "success",
+      pageNum: 1,
+      pages: 1,
+      list: data
+    }).end()
+  })
+})
 module.exports = productsApp;
