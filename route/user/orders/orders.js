957f6e7c83c8fd4ba8a4c36804305dbcbfdb9a1c
wen <1830759297@qq.com>
2019-04-12 01:17:00 +0800

    修改用户信息接口完成

--- route/user/orders/orders.js
+++ route/user/orders/orders.js
@@ -7,18 +7,33 @@ const queryProm = require('../../../util/queryProm');
 ordersApp.use(bodyParser.urlencoded({ extended: false }))
 ordersApp.use(bodyParser.json())
 
+//这是解析出来的token的值
+let decode = null;
+// 监听所有需要验证token的请求
+ordersApp.use(async function (req, res, next) {
+  if (req.method.toLowerCase() == 'options') {
+    res.send({
+      resultCode: 0,
+      resultMsg: "success"
+    }).end();
+  } else {
+    let urlArr = ['/']
+    if (urlArr.indexOf(req.url) != -1) { //请求地址存在于上面的数组中，则需要验证token值
+      decode = await Util.analyToken(req.get("auth"))
+      if (!decode) {  //如果token过期
+        console.log('已过期或者无效的token');
+        res.status(401).send({}).end();
+        return;
+      }
+      next();
+    } else { //如果不需要验证token，直接next
+      next();
+    }
+  }
+})
+
 //获取订单数量的接口
 ordersApp.get('/', async (req, res) => {
-  let decode = await Util.analyToken(req.get("auth"))
-  if (!decode) {
-    console.log('已过期或者无效的token');
-    res.status(401).send({
-      resultCode: 0,
-      resultMsg: "success",
-      data: outputData
-    }).end();
-    return;
-  }
   let username = decode.username;
   console.log('获取用户订单数量token值有效，用户名：', username);
   let data = await queryProm(`select unPayOrder,unDeliveryOrder,unReceivedOrder from ordersNum where username='${username}'`)
--- route/user/user.js
+++ route/user/user.js
@@ -8,12 +8,36 @@ const ordersRoute = require('./orders/orders')
 usersApp.use(bodyParser.urlencoded({ extended: false }))
 usersApp.use(bodyParser.json())
 
+//这是解析出来的token的值
+let decode = null;
+// 监听所有需要验证token的请求
+usersApp.use(async function (req, res, next) {
+  if (req.method.toLowerCase() == 'options') {
+    res.send({
+      resultCode: 0,
+      resultMsg: "success"
+    }).end();
+  }else{
+    let urlArr = ['/userInfo']
+    if (urlArr.indexOf(req.url) != -1) { //请求地址存在于上面的数组中，则需要验证token值
+      decode = await Util.analyToken(req.get("auth"))
+      if (!decode) {  //如果token过期
+        console.log('已过期或者无效的token');
+        res.status(401).send({}).end();
+        return;
+      }
+      next();
+    }else{ //如果不需要验证token，直接next
+      next();
+    }
+  }
+})
+
 // 用户订单接口在orders路由完成
 usersApp.use('/orders', ordersRoute)
 
 //登录接口
 usersApp.post('/login', async (req, res) => {
-
   let dataArr = await queryProm(`select username,password from user where username='${req.body.username}'`);
   let outputData = {
     code: '-1',// -1 表示登录失败，其他的表示成功
@@ -33,7 +57,7 @@ usersApp.post('/login', async (req, res) => {
       resultMsg: '密码错误'
     }
   } else {
-    let token = Util.setToken({ username: req.body.username }, 1*60*60);
+    let token = Util.setToken({ username: req.body.username }, 10*60*60);
     outputData = {
       code: '1',// -1 表示登录失败，其他的表示成功
       resultMsg: '登录成功',
@@ -70,19 +94,10 @@ usersApp.post('/login', async (req, res) => {
 
 //获取用户个人信息接口
 usersApp.get('/userInfo', async (req, res) => {
-  let decode = await Util.analyToken(req.get("auth"))
-  if (!decode) {
-    console.log('已过期或者无效的token');
-    res.status(401).send({
-      resultCode: 0,
-      resultMsg: "success",
-      data: outputData
-    }).end();
-    return;
-  }
-  let username = decode.username;
-  console.log('获取用户个人信息token值有效，用户名：', username);
+  let username = req.query.userId;
+  console.log('获取用户个人信息token值有效');
   let data = await queryProm(`select username,photo_url,sex,birthday from userInfo where username='${username}'`)
+  console.log(data);
   data = data[0]
   outputData = {
     personalInfo: data
@@ -91,6 +106,21 @@ usersApp.get('/userInfo', async (req, res) => {
     resultCode: 0,
     resultMsg: "success",
     data: outputData
+  }).end();
+})
+
+//修改用户个人信息接口
+usersApp.put('/userInfo', async (req, res) => {
+  let username = decode.username;
+  console.log('修改用户个人信息token值有效，用户名：', username);
+
+  console.log(req.body);
+  let data = req.body,personalInfo=data.personalInfo;
+  await queryProm(`update userInfo set username='${personalInfo.username}',sex='${personalInfo.sex}',birthday='${personalInfo.birthday}',photo_url='${personalInfo.photo_url}' where username='${data.id}'`)
+  res.send({
+    resultCode: 0,
+    resultMsg: "success",
+    data: {msg: '修改成功'}
   }).end();
 })
 
